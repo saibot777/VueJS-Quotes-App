10c4145f745a683623ec528489e90301489aa354
saibot777 <stefantrajkovic777@gmail.com>
2017-02-21 15:26:51 +0100

    initial commit

---
+++ quotes-app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import QuoteGrid from './components/QuoteGrid.vue';
+
+export default {
+  data: function() {
+    return {
+      quotes: [
+        'Just a Quote to see something'
+      ],
+      maxQuotes: 10
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+      <app-new-quote></app-new-quote>
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
+</template>
+
+<script>
+
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+
+export default {
+  data: function() {
+    return {
+      quotes: [
+        'Just a Quote to see something'
+      ],
+      maxQuotes: 10
+    }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote
+  }
+}
+</script>
+
+<style>
+
+</style>
