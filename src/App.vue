c843d68d67132e113d78b80268506ad0ae134e31
saibot777 <stefantrajkovic777@gmail.com>
2017-02-21 15:39:31 +0100

    creating quotes option added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="container">
-      <app-new-quote></app-new-quote>
+      <app-new-quote @quoteAdded="newQuote"></app-new-quote>
       <app-quote-grid :quotes="quotes"></app-quote-grid>
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <div class="alert alert-info">
+            Info: Click on a Quote to delete it!
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
@@ -17,6 +24,11 @@ export default {
         'Just a Quote to see something'
       ],
       maxQuotes: 10
+    }
+  },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote);
     }
   },
   components: {
